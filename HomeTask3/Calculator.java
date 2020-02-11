f02b04f53bed9086c04b1d370bcca01d391744d7
AnumariAnnapoorna <anumari.annapoorna@gmail.com>
2020-02-10 22:29:39 -0500

    commit

--- HomeTask3/Calculator.java
+++ HomeTask3/Calculator.java
@@ -6,12 +6,10 @@ public static void main(String args[])
 {
 MathFunctions c=new MathFunctions();
 Scanner in=new Scanner(System.in);
+int ch;
+float x,y;
 while(true)
 {
-System.out.println("Enter 1st number");
-int x=in.nextInt();
-System.out.println("Enter 2nd number");
-int y=in.nextInt();
 System.out.println("Menu");
 System.out.println("1.Addition");
 System.out.println("2.Multiplication");
@@ -19,7 +17,13 @@ System.out.println("3.Division");
 System.out.println("4.Substraction");
 System.out.println("5.exit");
 System.out.println("Enter your choice");
-int ch=in.nextInt();
+ch=in.nextInt();
+if(ch>=1 && ch<=4)
+{
+System.out.println("Enter 1st number");
+x=in.nextFloat();
+System.out.println("Enter 2nd number");
+y=in.nextFloat();
 switch(ch)
 {
 case 1:System.out.println("Addition:"+c.add(x,y));
@@ -30,7 +34,11 @@ case 3:System.out.println("Division:"+c.div(x,y));
 break;
 case 4:System.out.println("Substraction:"+c.sub(x,y));
 break;
-default:System.out.println("invalid option");
+}
+}
+else
+{
+System.out.println("invalid option");
 System.exit(0);
 }
 }
--- HomeTask3/MathFunctions.java
+++ HomeTask3/MathFunctions.java
@@ -1,18 +1,18 @@
 public class MathFunctions
 {
-	public int add(int a,int b)
+	public float add(float a,float b)
 	{
 		return a+b;
 	}
-	public int mul(int a,int b)
+	public float mul(float a,float b)
 	{
 		return a*b;
 	}
-	public int div(int a,int b)
+	public float div(float a,float b)
 	{
 		return a/b;
 	}
-	public int sub(int a,int b)
+	public float sub(float a,float b)
 	{
 		return a-b;
 	}
